da810baa8bbc885f96f0ab59d8f0b3bf48dbb099
jacoblimzm <jacob_lim@live.com>
2021-06-17 14:24:48 +0800

    feat: complete NavBar scaffold

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,18 +1,30 @@
-import { UserIcon } from "@heroicons/react/solid"
+import { UserIcon } from "@heroicons/react/solid";
 
 const NavBar = () => {
-    return ( 
-        <div className="bg-blue-600 min-h-screen text-blue-100 w-56">
-
-        <a href="#">
-            <UserIcon  className="w-24 h-24"/>
-            <span className="">Xanpool</span>
-        </a>
-        <nav>
-
-        </nav>
+  return (
+    <div className="bg-purple-600 min-h-screen text-blue-100 w-56 py-4 px-2">
+      <div className="flex">
+        <div className="mx-auto space-y-2 mb-4">
+          <UserIcon className="w-24 h-24 text-white" />
+          <p className="text-2xl font-bold">Xanpool</p>
         </div>
-     );
-}
- 
+      </div>
+
+      <nav>
+          <ul>
+              <li  className="py-2.5 px-4 hover:bg-blue-500 hover:text-white rounded">
+                  <a href="#">Accounts</a>
+              </li>
+              <li  className="py-2.5 px-4 hover:bg-blue-500 hover:text-white rounded">
+                  <a href="#">Settings</a>
+              </li>
+              <li  className="py-2.5 px-4 hover:bg-blue-500 hover:text-white rounded">
+                  <a href="#">Contact</a>
+              </li>
+          </ul>
+      </nav>
+    </div>
+  );
+};
+
 export default NavBar;
