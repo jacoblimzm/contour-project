e895b0395a5a00ff95c4011ea42bbf8970e8edbc
jacoblimzm <jacob_lim@live.com>
2021-06-18 01:02:39 +0800

    fix: remove unused code

--- client/src/components/TransactionListings.jsx
+++ client/src/components/TransactionListings.jsx
@@ -1,6 +1,8 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import AccountHeader from "./AccountHeader";
+import TransactionRow from "./TransactionRow";
 
 const TransactionListings = () => {
   const [accountState, setAccountState] = useState([]);
@@ -34,22 +36,9 @@ const TransactionListings = () => {
   }, [id]);
   return (
     <div className="flex-1 divide-y-2 divide-blue-500 px-2 bg-purple-100 ">
-      <div className="flex justify-between">
-        <div className="text-2xl font-bold p-10">
-          <h2 className="">
-            {accountState[0] && accountState[0].account_name} / {accountState[0] && accountState[0].currency}
-            <span className="block italic text-gray-500">
-              {accountState[0] && accountState[0].account_number} 
-            </span>
-          </h2>
-        </div>
-        <div className="text-2xl font-bold p-10">
-          <h2 className="">
-            Balance: ${accountState[0] && accountState[0].balance}
-            <span className="block text-gray-500">{accountState[0] && accountState[0].is_active ? "Active" : "Not Active"}</span>
-          </h2>
-        </div>
-      </div>
+    {accountState.map( account => {
+        return <AccountHeader key={account.id} {...account} />
+    })}
       <div className="p-4">
         <h1 className="text-3xl mb-4">Transactions</h1>
         <table className="min-w-full divide-y divide-gray-200">
@@ -79,35 +68,16 @@ const TransactionListings = () => {
               >
                 Amount
               </th>
+              <th scope="col" className="relative px-6 py-3">
+                    <span className="sr-only">View</span>
+                  </th>
+
             </tr>
           </thead>
           <tbody className="bg-white divide-y divide-gray-200">
-            {/* map function here   */}
-            <tr>
-              <td className="px-6 py-4 whitespace-nowrap">
-                <div className="flex items-center">
-                  <div className="">
-                    <div className="text-sm font-medium text-gray-900">
-                      12 May
-                    </div>
-                  </div>
-                </div>
-              </td>
-              <td className="px-6 py-4 whitespace-nowrap">
-                <div className="text-sm text-gray-900">
-                  adfadfsdfsdfsdfssdfsdsdfsfsdfsf
-                </div>
-              </td>
-              <td className="px-6 py-4 whitespace-nowrap">
-                <span className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
-                  Yes
-                </span>
-              </td>
-              <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                $200
-              </td>
-            </tr>
-            {/* to here */}
+            {transactionsState.map(transaction => {
+                return <TransactionRow key={transaction.id} {...transaction} />
+            })}
           </tbody>
         </table>
       </div>
