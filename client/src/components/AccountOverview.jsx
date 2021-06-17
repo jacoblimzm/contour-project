d8ecc060a035fd94d3de8cf811616294741d74ba
jacoblimzm <jacob_lim@live.com>
2021-06-17 20:50:55 +0800

    feat: complete component

--- client/src/components/AccountOverview.jsx
+++ client/src/components/AccountOverview.jsx
@@ -1,18 +1,31 @@
-const AccountOverview = ( {id, account_name, account_type, balance, currency, account_number, is_active} ) => {
+const AccountOverview = ({
+  id,
+  account_name,
+  account_type,
+  balance,
+  currency,
+  account_number,
+  is_active,
+}) => {
   return (
-    <div className="flex flex-row justify-between m-4 border-b-1">
-      <h2>
+    <div className="flex flex-row m-4  border-b border-gray-300">
+      <h2 className="flex-1">
         {account_name}
-        <span className="block italic text-gray-500">
-          {account_number}
-        </span>
+        <span className="block italic text-gray-500">{account_number}</span>
       </h2>
-      <h2>{account_type}</h2>
-      <h2>{currency}</h2>
-      <h2>{balance}</h2>
+      <h2 className="flex-1">
+        <span className="block">Account Type:</span>
+        {account_type}
+      </h2>
+      <h2 className="flex-1">
+        <span className="block">Currency:</span>
+        {currency}
+      </h2>
+      <h2 className="flex-1">
+        <span className="block">Balance:</span>${balance}
+      </h2>
     </div>
   );
 };
 
 export default AccountOverview;
-
