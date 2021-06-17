f18e89a5950f3765083ef7157b789b61018c9fd9
jacoblimzm <jacob_lim@live.com>
2021-06-17 20:59:33 +0800

    feat: add react router

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
+import { Switch, Route } from "react-router-dom";
 import NavBar from './components/NavBar';
 import AccountHome from './components/AccountHome';
 
 function App() {
   return (
-    <div className="container flex">
+    <div className="container flex font-body">
     <NavBar />
     <AccountHome />
     </div>
