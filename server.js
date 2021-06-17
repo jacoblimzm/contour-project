0e5a36a820925ff50a91abc3e4023876df25d720
jacoblimzm <jacob_lim@live.com>
2021-06-17 10:49:58 +0800

    fix: format code

--- server.js
+++ server.js
@@ -1,18 +1,18 @@
 require("dotenv").config();
 const express = require("express");
 
-
 // --------------------------------------- CONSTANTS ---------------------------------------
 const app = express();
-const PORT = (process.env.PORT || 8000);
+const PORT = process.env.PORT || 8000;
 
 // --------------------------------------- MIDDLEWARE ---------------------------------------
 app.use(express.json());
 
-
+const accountsController = require("./controllers/accountsController");
+app.use("/api", accountsController);
 
 // --------------------------------------- CONNECTIONS ---------------------------------------
 
 app.listen(PORT, () => {
-    console.log(`server is now listening at port ${PORT}`)
-})
+  console.log(`server is now listening at port ${PORT}`);
+});
