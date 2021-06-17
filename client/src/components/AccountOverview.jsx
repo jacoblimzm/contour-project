fd113a4310a81a5b8228ab2308744c8790503d97
jacoblimzm <jacob_lim@live.com>
2021-06-17 23:42:01 +0800

    feat: add Link to component

--- client/src/components/AccountOverview.jsx
+++ client/src/components/AccountOverview.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 const AccountOverview = ({
   id,
   account_name,
@@ -10,8 +11,18 @@ const AccountOverview = ({
   return (
     <div className="flex flex-row m-4  border-b border-gray-300">
       <h2 className="flex-1">
-        {account_name}
-        <span className="block italic text-gray-500">{account_number}</span>
+        <Link className="hover:text-white hover:bg-blue-500 rounded px-2" to={{
+          pathname: `/transactions/${id}`,
+          state: {
+            account_name,
+            account_number,
+            balance,
+            id,
+            is_active
+          }
+        }
+        }>{account_name}</Link>
+        <span className="px-2 block italic text-gray-500">{account_number}</span>
       </h2>
       <h2 className="flex-1">
         <span className="block">Account Type:</span>
