1a2448e91bd5227d89e2314d330f70acec3fc8e7
jacoblimzm <jacob_lim@live.com>
2021-06-18 14:32:03 +0800

    fix: add deployment code

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
-const app = require("./app")
+const app = require("./app");
 
 // --------------------------------------- CONSTANTS ---------------------------------------
 const PORT = process.env.PORT || 8000;
 
 // --------------------------------------- CONNECTIONS ---------------------------------------
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("./client/build"));
+}
 
 app.listen(PORT, () => {
   console.log(`server is now listening at port ${PORT}`);
