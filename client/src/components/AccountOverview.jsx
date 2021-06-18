d22ec1441753d043cacd598138013304bd9404c4
jacoblimzm <jacob_lim@live.com>
2021-06-18 09:16:00 +0800

    feat: add view button

--- client/src/components/AccountOverview.jsx
+++ client/src/components/AccountOverview.jsx
@@ -11,18 +11,25 @@ const AccountOverview = ({
   return (
     <div className="flex flex-row m-4  border-b border-gray-300">
       <h2 className="flex-1">
-        <Link className="hover:text-white hover:bg-blue-500 rounded px-2" to={{
-          pathname: `/transactions/${id}`,
-          state: {
-            account_name,
-            account_number,
-            balance,
-            id,
-            is_active
-          }
-        }
-        }>{account_name}</Link>
-        <span className="px-2 block italic text-gray-500">{account_number}</span>
+        {account_name}
+        <Link
+          className="text-indigo-600 hover:text-indigo-900 mx-2"
+          to={{
+            pathname: `/transactions/${id}`,
+            state: {
+              account_name,
+              account_number,
+              balance,
+              id,
+              is_active,
+            },
+          }}
+        >
+          View
+        </Link>
+        <span className="block italic text-gray-500">
+          {account_number}
+        </span>
       </h2>
       <h2 className="flex-1">
         <span className="block">Account Type:</span>
