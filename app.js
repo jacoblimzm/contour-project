59293e67df63092298483a69055fbcbd37bf1309
jacoblimzm <jacob_lim@live.com>
2021-06-18 14:38:07 +0800

    fix: middleware wrongly loaded in server

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ const app = express();
 // --------------------------------------- MIDDLEWARE ---------------------------------------
 app.use(express.json());
 
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("./client/build"));
+  }
+
 // --------------------------------------- CONTROLLERS ---------------------------------------
 const accountsController = require("./controllers/accountsController");
 app.use("/api", accountsController);
--- server.js
+++ server.js
@@ -5,10 +5,6 @@ const PORT = process.env.PORT || 8000;
 
 // --------------------------------------- CONNECTIONS ---------------------------------------
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("./client/build"));
-}
-
 app.listen(PORT, () => {
   console.log(`server is now listening at port ${PORT}`);
 });
